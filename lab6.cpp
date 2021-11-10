9fbaf529973850c11fefa609ecff7475280477bd
evelinalevina <90408529+evelinalevina@users.noreply.github.com>
2021-11-10 22:42:39 +0200

    Add files via upload

---
+++ lab6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "functions.hpp"
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Введите количество строк и столбцов: ";
+    int n;
+    int arr[100][100];
+    el::Read(n, arr);
+    if (el::ProverkaUsloviya(n, arr))
+    {
+        el::Sort(n, arr);
+        el::Write(n, arr);
+    }
+    else
+    {
+        std::cout << "Не выполнено условие! ";
+    }
+    return 0;
+
+}
+
+
