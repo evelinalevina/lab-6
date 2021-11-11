ab86b8f33188923f8c9c8b042cc2638fb3ba8c31
evelinalevina <edlevina@stud.kantiana.ru>
2021-11-11 10:39:40 +0200

    добавлены файлы для функций и CMake

---
+++ functions.cpp
@@ -0,0 +1,123 @@
+#include "functions.hpp"
+#include <math.h>
+#include <fstream>
+#include <iostream>
+
+namespace el
+{
+    bool isPrime(int x)
+    {
+        if (x < 2)
+            return false;
+        for (int d = 2; d <= sqrt(x); d++)
+            if (x % d == 0)
+                return false;
+        return true;
+    }
+
+    void Read(int& n, int arr[100][100])
+    {
+        std::cin >> n;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                std::cin >> arr[i][j];
+    }
+
+    bool ConsistsPrime(int n, int arr[100][100])
+    {
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (isPrime(arr[i][j]))
+                    return true;
+        return false;
+    }
+
+    void Write(int n, int arr[100][100])
+    {
+        std::cout << "Результат: " << std::endl;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                std::cout << arr[i][j] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+    
+
+    
+
+    void Sort(int n, int arr[100][100])
+    {
+        int sum[100];
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = j + 1; k < n; ++k)
+            {
+                for (int i = 0; i < n-1; i++)
+                {
+                    sum[j] += arr[i][j];                   
+                }
+                if (sum[j] > sum[k])
+                {
+                    for (int i = 0; i < n; i++)
+                        std::swap(arr[i][j], arr[i][k]);
+                }
+            }           
+        }
+
+       
+    }
+
+    bool ProverkaUsloviya(int n, int arr[100][100]) {
+        if (Equal(n, arr) && Sum(n, arr)) {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool Sum(int n, int arr[100][100]) {
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum = sum + arr[i][j];
+            }
+
+            if (sum == 0) {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+
+    bool Equal(int n, int arr[100][100]) {
+        for (int i = 0; i < n; i++) {
+            int flag_a = 0;
+            for (int j = 0; j < n; j++)
+            {
+                int flag_b = 1;
+                if (arr[i][j] < 2)
+                    flag_b = 0;
+                for (int d = 2; d <= sqrt(arr[i][j]); d++)
+                    if (arr[i][j] % d == 0)
+                        flag_b = 0;
+                if (flag_b == 1)
+                    flag_a = 1;
+                if (flag_a == 1) {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+
+    
+
+}
---
+++ functions.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace el
+{
+    bool isPrime(int x);
+    
+    void Read(int& n, int arr[100][100]);
+    
+    bool ConsistsPrime(int n,  int arr[100][100]);
+    
+    void Write(int n,  int arr[100][100]);
+
+    void Sort(int n,  int arr[100][100]);
+
+    bool ProverkaUsloviya(int n, int arr[100][100]);
+
+    bool Sum(int n, int arr[100][100]);
+
+    bool Equal(int n, int arr[100][100]);
+   
+    
+}
